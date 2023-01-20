f12aa15f63e2806fd07162aa9ffd4a27bc14e0be
Nicolas Morin <nicolas.henryk.morin@gmail.com>
2023-01-20 09:35:05 -0500

    renamed migration file

--- veriscope_ta_dashboard/database/migrations/2023_01_10_142448_add_national_column_to_trust_anchors_table.php
+++ veriscope_ta_dashboard/database/migrations/2023_01_10_142448_add_national_column_to_trust_anchors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnToTrustAnchorsTable01 extends Migration
+class AddNationalColumnToTrustAnchorsTable extends Migration
 {
     /**
      * Run the migrations.
